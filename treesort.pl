fa8bb84ef62b67e04115b05d5c1efbcd4d97781d
punfil <77413489+punfil@users.noreply.github.com>
2021-11-11 15:31:58 +0100

    Create treesort.pl

---
+++ treesort.pl
@@ -0,0 +1,21 @@
+dolacz( [],L,L ). 
+dolacz( [H|T],L,[H|X] ) :- dolacz( T,L,X ).
+
+add(X,nil,t(X,nil,nil)).
+add(X,t(Root,L,R),t(Root,L1,R)) :- X < Root, add(X,L,L1).
+add(X,t(Root,L,R),t(Root,L,R1)) :- X >= Root, add(X,R,R1).
+
+
+construct(L, T) :- construct(L,T,nil).
+
+construct([],T,T).
+construct([N|Ns],T,T0) :- add(N,T0,T1), construct(Ns,T,T1).
+
+traverse(nil, []).
+traverse(t(X, Left, Right), C) :- 
+	traverse(Left, R1),
+	traverse(Right, R2),
+	dolacz([X], R2, N),
+	dolacz(R1, N, C).
+
+tree_sort(L, X) :- construct(L, T), traverse(T, X).
