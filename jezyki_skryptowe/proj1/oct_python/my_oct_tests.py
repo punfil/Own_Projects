3b589c576dfc5689228b1be2a4cdf769b400d64c
punfil <wojtek.panfil@hotmail.com>
2024-02-25 22:29:28 +0100

    skryptowe: Add python performance test
    Signed-off-by: punfil <wojtek.panfil@hotmail.com>

--- jezyki_skryptowe/proj1/oct_python/my_oct_tests.py
+++ jezyki_skryptowe/proj1/oct_python/my_oct_tests.py
@@ -4,6 +4,7 @@ import os
 import pytest
 import subprocess
 import concurrent.futures
+import time
 from test_data_generator import generate_N_numbers, NUMBERS_PATH
 
 from my_oct import my_oct
@@ -59,6 +60,7 @@ def test_c_single_number(generate_single_number):
     assert oct_num in get_c_result(generate_single_number)
 
 
+@pytest.mark.skip("Function tested")
 def test_python_generated_numbers(load_generated_numbers):
     assert load_generated_numbers
 
@@ -66,8 +68,32 @@ def test_python_generated_numbers(load_generated_numbers):
         if oct(number) != my_oct(number):
             raise ValueError("Improper value %d from %d", my_oct(number), number)
 
+
+@pytest.mark.skip(reason="Function tested")
 def test_c_generated_numbers(load_generated_numbers):
     assert load_generated_numbers
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.map(compare_octals, load_generated_numbers)
+
+
+def test_python_time_accurancy():
+    LOGGER.log(logging.INFO, "%0.9f", time.time_ns() - time.time_ns())
+
+
+def test_python_performance(load_generated_numbers):
+    N = 1
+    assert load_generated_numbers
+
+    start = time.time_ns()
+    for _ in range(N):
+        for dec_num in load_generated_numbers:
+            my_oct(dec_num)
+    checkpoint_1 = time.time_ns()
+
+    for _ in range(N):
+        for dec_num in load_generated_numbers:
+            pass
+    end = time.time_ns()
+
+    LOGGER.log(logging.INFO, "Calc loop exec_time: %.9f, empty loop exec_time: %.9f", (checkpoint_1 - start) / 1_000_000_000, (end - checkpoint_1) / 1_000_000_000)
