49e2b6407fa9cc21f7b25acb844a2b751176638b
punfil <wojtek.panfil@hotmail.com>
2024-02-25 22:05:27 +0100

    skryptowe: Make generated numbers unique
    Signed-off-by: punfil <wojtek.panfil@hotmail.com>

--- jezyki_skryptowe/proj1/oct_python/test_data_generator.py
+++ jezyki_skryptowe/proj1/oct_python/test_data_generator.py
@@ -5,6 +5,10 @@ MIN_C_VAL = -9223372036854775808
 MAX_C_VAL = 9223372036854775807
 
 def generate_N_numbers(N):
-    numbers = [f"{randint(MIN_C_VAL, MAX_C_VAL)}\n" for _ in range(N)]
+    uniq_num = set()
+    while len(uniq_num) < N:
+        uniq_num.add(randint(MIN_C_VAL, MAX_C_VAL))
+
+    numbers = [f"{num}\n" for num in uniq_num]
     with open("../numbers_generated.txt", "w") as f:
         f.writelines(numbers)
