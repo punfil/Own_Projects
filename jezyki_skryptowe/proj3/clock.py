a78eb0024a83986e463c7d19e5d72e03b9eb7471
punfil <wojtek.panfil@hotmail.com>
2024-04-02 22:29:17 +0200

    Add proj3 initial
    Signed-off-by: punfil <wojtek.panfil@hotmail.com>

---
+++ jezyki_skryptowe/proj3/clock.py
@@ -0,0 +1,22 @@
+import sys
+import argparse
+
+from clock_qt import launch_qt
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Uruchom aplikację zegara.')
+    parser.add_argument('--qt', '-q', action='store_true', help='Uruchom aplikację z interfejsem w PyQT5.')
+    parser.add_argument('--gtk', '-g', action='store_true', help='Uruchom aplikcją z interfejsem w PyGTK3.')
+    args = parser.parse_args()
+
+    if args.qt:
+        launch_qt()
+    elif args.gtk:
+        print("Do nothing with GTK for now.")
+        sys.exit(0)
+
+    launch_qt()
+
+if __name__ == '__main__':
+    main()
---
+++ jezyki_skryptowe/proj3/clock_qt.py
@@ -0,0 +1,188 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QComboBox, QPushButton, QTabWidget, QTimeEdit, QMessageBox, QAction
+from PyQt5.QtCore import QTimer, QDateTime, Qt, QTime
+import pytz
+
+
+FONT_SIZE = 20
+
+
+class ClockAppQT5(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('184657 Panfil Wojciech - Języki skryptowe 3 - Zegar')
+        self.setGeometry(100, 100, 800, 600)
+
+        # Tab Widget
+        self.tab_widget = QTabWidget()
+        self.clock_tab = QWidget()
+        self.stopwatch_tab = QWidget()
+        self.timer_tab = QWidget()
+        self.help_tab = QWidget()
+
+        # Clock Tab
+        self.clock_layout = QVBoxLayout()
+        self.current_time_label = QLabel()
+        self.current_time_label.setAlignment(Qt.AlignCenter)
+        self.current_time_label.font().setPointSize(FONT_SIZE)
+        self.clock_layout.addWidget(self.current_time_label)
+
+        self.timezones = pytz.common_timezones
+
+        self.timezone_combo = QComboBox()
+        self.timezone_combo.addItems(self.timezones)
+        self.timezone_combo.currentIndexChanged.connect(self.update_time)
+
+        self.clock_layout.addWidget(self.timezone_combo)
+        self.clock_tab.setLayout(self.clock_layout)
+        self.tab_widget.addTab(self.clock_tab, "Zegar")
+
+        # Stopwatch Tab
+        self.stopwatch_layout = QVBoxLayout()
+        self.stopwatch_label = QLabel("00:00:00")
+        self.stopwatch_label.setAlignment(Qt.AlignCenter)
+        self.stopwatch_label.font().setPointSize(FONT_SIZE)
+        self.stopwatch_layout.addWidget(self.stopwatch_label)
+
+        self.start_stopwatch_button = QPushButton("Rozpocznij")
+        self.stop_stopwatch_button = QPushButton("Zatrzymaj")
+        self.reset_stopwatch_button = QPushButton("Wyczyść")
+
+        self.start_stopwatch_button.clicked.connect(self.start_stopwatch)
+        self.stop_stopwatch_button.clicked.connect(self.stop_stopwatch)
+        self.reset_stopwatch_button.clicked.connect(self.reset_stopwatch)
+
+        self.stopwatch_layout.addWidget(self.start_stopwatch_button)
+        self.stopwatch_layout.addWidget(self.stop_stopwatch_button)
+        self.stopwatch_layout.addWidget(self.reset_stopwatch_button)
+
+        self.stopwatch_tab.setLayout(self.stopwatch_layout)
+        self.tab_widget.addTab(self.stopwatch_tab, "Stoper")
+
+        # Timer Tab
+        self.timer_layout = QVBoxLayout()
+
+        self.timer_edit = QTimeEdit()
+        self.timer_edit.setDisplayFormat("hh:mm:ss")
+        self.timer_edit.setTime(QTime(0, 0))
+        self.timer_edit.font().setPointSize(FONT_SIZE)
+        self.timer_layout.addWidget(self.timer_edit)
+
+        self.start_timer_button = QPushButton("Rozpocznij")
+        self.start_timer_button.clicked.connect(self.start_timer)
+        self.timer_layout.addWidget(self.start_timer_button)
+
+        self.stop_timer_button = QPushButton("Zatrzymaj")
+        self.stop_timer_button.clicked.connect(self.stop_timer)
+        self.timer_layout.addWidget(self.stop_timer_button)
+
+        self.timer_tab.setLayout(self.timer_layout)
+        self.tab_widget.addTab(self.timer_tab, "Timer")
+
+        # Help Tab
+        self.help_tab = QWidget()
+        self.help_layout = QVBoxLayout()
+        self.help_text = QLabel()
+        self.help_text.setText("Clock, Stopwatch, and Timer App\n\n"
+                               "This application allows you to:\n"
+                               "- View the current time in different timezones.\n"
+                               "- Use a stopwatch to measure time intervals.\n"
+                               "- Set a timer for countdown.\n\n"
+                               "Created using PyQt5 by [Your Name]")
+        self.help_layout.addWidget(self.help_text)
+        self.help_tab.setLayout(self.help_layout)
+        self.tab_widget.addTab(self.help_tab, "About")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.tab_widget)
+        self.setLayout(layout)
+
+        # Clock
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+        self.update_time()
+
+        # Stopwatch
+        self.stopwatch_timer = QTimer()
+        self.stopwatch_timer.timeout.connect(self.tick_stopwatch)
+        self.stopwatch_running = False
+        self.stopwatch_seconds = 0
+
+        # Timer
+        self.timer_timer = QTimer()
+        self.timer_timer.timeout.connect(self.update_timer)
+        self.timer_running = False
+
+    def update_time(self):
+        current_timezone = pytz.timezone(self.timezone_combo.currentText())
+        current_time = QDateTime.currentDateTime().toPyDateTime()
+        local_time = current_time.astimezone(current_timezone)
+        local_time_str = local_time.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+
+        # Display the local time
+        self.current_time_label.setText(local_time_str)
+
+    def start_stopwatch(self):
+        self.stopwatch_running = True
+        self.stopwatch_timer.start(1000)
+
+    def stop_stopwatch(self):
+        self.stopwatch_running = False
+        self.stopwatch_timer.stop()
+
+    def reset_stopwatch(self):
+        self.stopwatch_running = False
+        self.stopwatch_timer.stop()
+        self.stopwatch_seconds = 0
+        self.update_stopwatch()
+
+    def tick_stopwatch(self):
+        self.stopwatch_seconds += 1
+        self.update_stopwatch()
+
+    def update_stopwatch(self):
+        seconds = self.stopwatch_seconds % 60
+        minutes = (self.stopwatch_seconds // 60) % 60
+        hours = self.stopwatch_seconds // 3600
+        stopwatch_time = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+        self.stopwatch_label.setText(stopwatch_time)
+
+    def start_timer(self):
+        if not self.timer_running:
+            self.timer_running = True
+            self.timer_end_time = QDateTime.currentDateTime().time().addSecs(QTime(0, 0).secsTo(self.timer_edit.time()))
+            self.timer_timer.start(1000)
+
+    def stop_timer(self):
+        self.timer_running = False
+        self.timer_timer.stop()
+        self.timer_edit.setTime(QTime(0, 0))
+
+    def update_timer(self):
+        current_time = QDateTime.currentDateTime().time()
+        if current_time >= self.timer_end_time or not self.timer_running:
+            self.stop_timer()
+        else:
+            remaining_time = current_time.secsTo(self.timer_end_time)
+            display_time = QTime(0, 0).addSecs(remaining_time)
+            self.timer_edit.setTime(display_time)
+
+    def show_about_dialog(self):
+        about_message = QMessageBox()
+        about_message.setWindowTitle('O aplikacji')
+        about_message.setText('Clock, Stopwatch, and Timer App\n\n'
+                              'This application allows you to:\n'
+                              '- View the current time in different timezones.\n'
+                              '- Use a stopwatch to measure time intervals.\n'
+                              '- Set a timer for countdown.\n\n'
+                              'Created using PyQt5 by [Your Name]')
+        about_message.setIcon(QMessageBox.Information)
+        about_message.exec_()
+
+
+def launch_qt():
+    app = QApplication(sys.argv)
+    clock_app = ClockAppQT5()
+    clock_app.show()
+    sys.exit(app.exec_())
